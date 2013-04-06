a5943244f59d7784a3073791a187f63ad5f5390b
JoeForshaw <joe.d.forshaw@gmail.com>
2013-04-06 11:56:21 +0100

    Refactored missing technology functionality

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+
+  def missing_technology_link
+    if user_signed_in?
+      return mail_to("tuutorials@gmail.com", "Something missing?", :id => "missing-technology", :subject => "Add a new technology to the list")
+    else
+     return link_to("Something missing?", :new_user_session, :id => "missing-technology")
+    end
+  end
+
 end
