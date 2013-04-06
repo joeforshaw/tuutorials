d21529dcd35a60db8458dc7acb5694508047029a
JoeForshaw <joe.d.forshaw@gmail.com>
2013-04-06 12:35:46 +0100

    Changed email in mail to

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
 
   def missing_technology_link
     if user_signed_in?
-      return mail_to("tuutorials@gmail.com", "Something missing?", :id => "missing-technology", :subject => "Add a new technology to the list")
+      return mail_to("tuuutorials@gmail.com", "Something missing?", :id => "missing-technology", :subject => "Add a new technology to the list")
     else
      return link_to("Something missing?", :new_user_session, :id => "missing-technology")
     end
